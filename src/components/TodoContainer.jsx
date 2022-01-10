44cf526df9091778a0bb98aa517738737f295121
Fabricio Garcia <fabgrel10@gmail.com>
2022-01-10 14:46:00 -0600

    ✨ feat: Complete Part 1

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import Header from './Header';
+import TodosList from './TodoList';
 
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false
+      }
+    ]
+  };
+
   render() {
     return (
       <div>
-        <h1>Hello from Vite App</h1>
-        <p>I am in a React Component!</p>
+        <Header />
+        <TodosList todos={this.state.todos} />
       </div>
     );
   }
