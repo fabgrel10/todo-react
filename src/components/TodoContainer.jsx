6e1e7ab18b9f1a469cfb992647c2c2ddef78637d
Fabricio Garcia <fabgrel10@gmail.com>
2022-01-11 13:42:50 -0600

    ✨ feat: Complete Part 2

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,33 +1,69 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 import Header from './Header';
+import InputTodo from './InputTodo';
 import TodosList from './TodoList';
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: 'Setup development environment',
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: 'Develop website and add content',
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: 'Deploy to live server',
         completed: false
       }
     ]
   };
 
+  handleChange = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    }));
+  };
+
+  addTodoItem = title => {
+    console.log(title);
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
       </div>
     );
   }
